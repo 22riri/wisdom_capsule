71cafb4f0c06d33f98b114a1613d15ba64c6c405
Philine Ross <philineross@icloud.com>
2024-06-05 12:23:37 +0100

    routes refactor

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -10,14 +10,22 @@ class MemoriesController < ApplicationController
     @memory.memorybox = @memorybox
 
     if @memory.save
-      redirect_to ownmemories_path
+      redirect_to ownmemories_path(@memorybox)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def ownmemories
-    @memories = current_user.memories
+    # TO DO : in case a user can have several memory boxes inside a time capsule, this needs to be extended
+    @memorybox = Memorybox.where(id: params[:memorybox_id], user: current_user)[0]
+
+    if @memorybox.present?
+      @time_capsule = Timecapsule.find(@memorybox.timecapsule_id)
+      @memories = @memorybox.memories
+    else
+      # alert
+    end
   end
 
   private
